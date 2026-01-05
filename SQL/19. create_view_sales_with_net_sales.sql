dcc780afd032c58a7690d05427bd2637e3ba78e8
irfanzim <irfanuzim08@gmail.com>
2026-01-05 12:18:22 +0300

    update_vw_net_sales to add coalesce

--- SQL/19. create_view_sales_with_net_sales.sql
+++ SQL/19. create_view_sales_with_net_sales.sql
@@ -3,12 +3,13 @@ Purpose:
 Create view to get net_sales by joining vw_sales_with_net_invoice_sales with post_invoice_deductions
 by using keys(customer_code, product_code, date)
 net_sales=net_invoice_sales*(1-discount_pct-other_deduction_pct)
+coalesce is done to avoid null value
 */
 create or replace view vw_sales_with_net_sales as 
 SELECT 
 	nis.*,
-    pod.discounts_pct,
-    pod.other_deductions_pct,
+    COALESCE(pod.discounts_pct,0) as discounts_pct,
+    coalesce(other_deductions_pct,0)as other_ded_pct,
     nis.net_invoice_sales*(1-pod.discounts_pct-pod.other_deductions_pct) as net_sales  
 FROM 
 	gdb041.vw_sales_with_net_invoice_sales nis
